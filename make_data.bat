59bf743dcd89c4824d1bccbacc971345f000bf1e
lcd11001 <lcd11001@gmail.com>
2020-09-20 23:46:51 +0700

    + excel parser tool

---
+++ make_data.bat
@@ -0,0 +1,8 @@
+@echo OFF
+
+SET CUR_DIR=%~dp0
+SET TOOL=%CUR_DIR%\tools\ExcelParser.js
+SET TEXT_DATA=%CUR_DIR%\data\text.xls
+SET OUTPUT=%CUR_DIR%\src\translations
+
+node %TOOL% %TEXT_DATA% %OUTPUT%
